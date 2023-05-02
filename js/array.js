d705302ba8e9df07786eb572ab2bc39562f962c9
Paulo Santos <102436341+pauloninja@users.noreply.github.com>
2023-05-02 06:42:28 -0300

    Atualizar

---
+++ js/array.js
@@ -0,0 +1,158 @@
+let products = {
+  data: [
+    {
+      productName: "Camisa Feminina Fashion Store",
+      category: "Bottomwear",
+      price: "49",
+      image: "img/camisa-1.jpg",
+    },
+    {
+      productName: "Camisa Feminina Fashion Store",
+      category: "Bottomwear",
+      price: "49",
+      image: "img/camisa-2.jpg",
+    },
+    {
+      productName: "Camisa Feminina Fashion Store",
+      category: "Bottomwear",
+      price: "49",
+      image: "img/camisa-3.jpg",
+    },
+    {
+      productName: "Camisa Feminina Fashion Store",
+      category: "Bottomwear",
+      price: "49",
+      image: "img/camisa-4.jpg",
+    },
+    {
+      productName: "Vestidos Feminina Fashion Store",
+      category: "Topwear",
+      price: "30",
+      image: "img/vestidos.jpg",
+    },
+    {
+      productName: "Vestidos Feminina Fashion Store",
+      category: "Bottomwear",
+      price: "49",
+      image: "img/vestidos-2.jpg",
+    },
+    {
+      productName: "Vestidos Feminina Fashion Store",
+      category: "Watch",
+      price: "99",
+      image: "img/vestidos-33.jpg",
+    },
+    {
+      productName: "Vestidos Feminina Fashion Store",
+      category: "Topwear",
+      price: "29",
+      image: "img/vestidos-4.jpg",
+    },
+    {
+      productName: "Moda Feminina Fashion Store",
+      category: "Topwear",
+      price: "29",
+      image: "img/sapato.jpg",
+    },
+    {
+      productName: "Moda Feminina Fashion Store",
+      category: "Topwear",
+      price: "29",
+      image: "img/sapato-2.jpg",
+    },
+    {
+      productName: "Moda Feminina Fashion Store",
+      category: "Topwear",
+      price: "29",
+      image: "img/sapato-3.jpg",
+    },
+    {
+      productName: "Moda Feminina Fashion Store",
+      category: "Topwear",
+      price: "29",
+      image: "img/sapato-4.jpg",
+    },
+    {
+      productName: "Camisa Masculina Fashion Store",
+      category: "Jacket",
+      price: "129",
+      image: "img/camisaM.jpg",
+    },
+    {
+      productName: "Camisa Masculina Fashion Store",
+      category: "Bottomwear",
+      price: "89",
+      image: "img/camisaM-2.jpg",
+    },
+    {
+      productName: "Camisa Masculina Fashion Store",
+      category: "Jacket",
+      price: "189",
+      image: "img/camisaM-3.jpg",
+    },
+    {
+      productName: "Camisa Masculina Fashion Store",
+      category: "Bottomwear",
+      price: "49",
+      image: "img/camisaM-4.jpg",
+    },
+
+    {
+      productName: "Moda Masculina Fashion Store",
+      category: "Jacket",
+      price: "129",
+      image: "img/calca.jpg",
+    },
+    {
+      productName: "Moda Masculina Fashion Store",
+      category: "Bottomwear",
+      price: "89",
+      image: "img/calca-2.jpg",
+    },
+    {
+      productName: "Moda Masculina Fashion Store",
+      category: "Jacket",
+      price: "189",
+      image: "img/calca-3.jpg",
+    },
+    {
+      productName: "Moda Masculina Fashion Store",
+      category: "Bottomwear",
+      price: "49",
+      image: "img/calca-4.jpg",
+    },
+    {
+      productName: "Tenis Fashion Store",
+      category: "Jacket",
+      price: "129",
+      image: "img/tenis.jpg",
+    },
+    {
+      productName: "Tenis Fashion Store",
+      category: "Bottomwear",
+      price: "89",
+      image: "img/tenis-2.jpg",
+    },
+    {
+      productName: "Tenis Fashion Store",
+      category: "Jacket",
+      price: "189",
+      image: "img/tenis-3.jpg",
+    },
+    {
+      productName: "Tenis Fashion Store",
+      category: "Bottomwear",
+      price: "49",
+      image: "img/tenis-4.jpg",
+    },
+   
+    
+  ],
+};
+
+for (let i of products.data){
+  //Criar cartão
+  let card = document.createElement("div");
+  //Cartão deve ser categoria  e deve ficar oculto inicialmente
+  card.classList.add
+}
