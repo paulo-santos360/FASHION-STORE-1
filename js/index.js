8db701191325c44fd679132f47022edb95adea98
Paulo Santos <102436341+pauloninja@users.noreply.github.com>
2023-05-07 16:38:50 -0300

    Atualizar

--- js/index.js
+++ js/index.js
@@ -7,4 +7,22 @@ function myFunction() {
     }
   } 
 
+  /*Animação*/
   AOS.init();
+
+  /*Rolagem até o Topo*/
+  const rolagem = ()=> {
+    const html = document.documentElement
+    const seta = document.getElementById('go-top')
+  
+    /* Se a rolagem for maior que 550, a seta aparece 
+    abaixo de 550 esconde */
+    if (html.scrollTop > 350 ) {
+      seta.style.display = 'block'
+    } else {
+      seta.style.display = 'none'
+    }
+  }
+
+  //window.addEventListener('scroll', rolagem)
+window.onscroll = ()=> rolagem()//evento somente para elem q existem no html
